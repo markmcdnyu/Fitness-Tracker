11c43e67cdc4296990897f8a9be27c4f3d5c3554
Mark McDonald <markwilliammcdonald@gmail.com>
2020-08-21 14:54:16 -0500

    added in the object properities

--- models/Workout.js
+++ models/Workout.js
@@ -4,18 +4,19 @@ const Schema = mongoose.Schema;
 
 const ExerciseSchema = new Schema({
     //need type
-
+    type: String,
     //need name
-
+    name: String,
     //need duration
-
+    duration: Number,
     //need distance
-
+    distance: Number,
     //need weight
-
+    weight: Number,
     //need reps
-
+    reps: Number,
     //need sets
+    sets: Number,
 });
 
 
